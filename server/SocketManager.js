46505baefec7611fea5dd7ab5c95db4976a04432
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-07-06 19:31:26 -0400

    Socket manager sort of working.

---
+++ server/SocketManager.js
@@ -0,0 +1,44 @@
+import { Server as SocketServer } from 'socket.io'
+
+export default class SocketManager {
+    constructor(httpServer) {
+        this.socketServer = new SocketServer(httpServer, {
+            cors: {
+                origin: '*',
+                methods: ['GET', 'POST'],
+            },
+        })
+    }
+
+    listen() {
+        this.socketServer.on('connection', (socket) => {
+            console.log('a user connected')
+
+            socket.on('disconnect', () => {
+                console.log('user disconnected')
+            })
+
+            socket.on('create game', () => {
+                // const gid = this.gameManager.createGame(socket)
+                // socket.join(gid)
+            })
+
+            socket.on('join', (gid) => {
+                console.log(`joining game ${gid}`)
+                socket.join(gid)
+            })
+
+            socket.on('send message', (msg, gid) => {
+                this.socketServer.to(gid).emit('receive message', msg)
+            })
+
+            socket.onAny((eventName, ...args) => {
+                console.log(eventName, args)
+            })
+
+            socket.onAnyOutgoing((eventName, ...args) => {
+                console.log(eventName, args)
+            })
+        })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,15 +3,11 @@ import {createServer} from 'node:http'
 import {fileURLToPath} from 'node:url'
 import {dirname, join} from 'node:path'
 import {Server} from 'socket.io'
+import SocketManager from './SocketManager.js'
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
-})
+const socketManager = new SocketManager(server)
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -19,30 +15,7 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'))
 })
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-
-    socket.on('join', (game_id) => {
-        console.log(`joining game ${game_id}`)
-        socket.join(game_id)
-    })
-
-    socket.on('send message', (msg, game) => {
-        io.to(game).emit('receive message', msg)
-    })
-
-    socket.onAny((eventName, ...args) => {
-        console.log(eventName, args)
-    })
-
-    socket.onAnyOutgoing((eventName, ...args) => {
-        console.log(eventName, args)
-    })
-})
+socketManager.listen()
 
 server.listen(3001, () => {
     console.log('server running at http://localhost:3001')
