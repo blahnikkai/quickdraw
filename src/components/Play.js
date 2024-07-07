493e794e309014a851009571b9821b404843c4e8
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-07-07 12:29:56 -0400

    Add form for entering a word in Play component.

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,9 +1,10 @@
 import io from 'socket.io-client'
 import { useParams } from 'react-router-dom'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Play() {
     const { gid } = useParams()
+    const [word, setWord] = useState('')
     const socketRef = useRef(null)
 
     useEffect(() => {
@@ -16,7 +17,13 @@ export default function Play() {
 
     return (
         <main>
-            
+            <div>Word placeholder</div>
+            <form onSubmit={(event) => event.preventDefault()}>
+                <input
+                    onChange={(event) => setWord(event.target.value)}
+                    value={word}
+                />
+            </form>
         </main>
     )
 }
