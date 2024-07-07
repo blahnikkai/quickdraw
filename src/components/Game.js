881d03138290df7e54e98824f26f0700d8d28bbb
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-07-07 00:13:37 -0400

    Delete code for chat rooms in Game component.

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,15 +7,9 @@ export default function Game() {
     const { gid } = useParams()
     const socketRef = useRef(null)
 
-    const [addingMsg, setAddingMsg] = useState('')
-    const [pastMsgs, setPastMsgs] = useState([])
-
     useEffect(() => {
         socketRef.current = io(':3001')
         socketRef.current.emit('join', gid)
-        socketRef.current.on('receive message', (newMsg) => {
-            setPastMsgs((pastMsgs) => [...pastMsgs, newMsg])
-        })
         return () => {
             socketRef.current.disconnect()
         }
@@ -23,22 +17,7 @@ export default function Game() {
 
     return (
         <main>
-            <div>Send a message</div>
-            <form
-                onSubmit={(event) => {
-                    event.preventDefault()
-                    socketRef.current.emit('send message', addingMsg, gid)
-                    setAddingMsg('')
-                }}
-            >
-                <input
-                    value={addingMsg}
-                    onChange={(event) => setAddingMsg(event.target.value)}
-                ></input>
-            </form>
-            <ul>
-                {pastMsgs.map((pastMsg, i) => <li key={i}>{pastMsg}</li>)}
-            </ul>
+            
         </main>
     )
 }
