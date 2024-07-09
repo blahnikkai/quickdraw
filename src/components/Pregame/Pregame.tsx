755e64db05aafdc9745078726fa04d4774feb0f9
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-07-09 16:06:41 -0400

    Add a label for nickname.

--- src/components/Pregame/Pregame.tsx
+++ src/components/Pregame/Pregame.tsx
@@ -1,12 +1,17 @@
 import { Socket } from 'socket.io-client'
+import './Pregame.css'
 
 export default function Pregame({ socket, name, gid }: { socket: Socket, name: string, gid: string }) {
     return (
         <div className='game-ui'>
-            <input
-                onChange={(event) => socket.emit('change name', gid, event.target.value)}
-                value={name}
-            />
+            <label className='nickname'>
+                Enter a nickname:
+                <input
+                    className='nickname'
+                    onChange={(event) => socket.emit('change name', gid, event.target.value)}
+                    value={name}
+                />
+            </label>
             <button
                 onClick={() => socket.emit('start game', gid)}
             >
