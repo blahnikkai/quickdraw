a041646f4e11b6aa938c1a803f4c1f3b6e0e9931
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-04-04 19:32:39 -0400

    Don't allow an empty nickname

--- src/components/Nickname/Nickname.tsx
+++ src/components/Nickname/Nickname.tsx
@@ -7,23 +7,29 @@ export default function Nickname({
     submitName: (name: string) => void;
 }) {
     const [name, setName] = useState("");
+    const [invalidName, setInvalidName] = useState(false);
     return (
         <form
             className="game-ui"
             onSubmit={(event) => {
                 event.preventDefault();
+                if (name === "") {
+                    setInvalidName(true);
+                    return;
+                }
+                setInvalidName(false);
                 submitName(name);
             }}
         >
             <label className="nickname">
                 Enter a nickname:
                 <input
-                    className="nickname"
+                    className={"nickname" + (invalidName ? " invalid-name" : "")}
                     onChange={(event) => setName(event.target.value)}
                     defaultValue={name}
                 />
             </label>
-            <button>Join Game</button>
+            <button>Join</button>
         </form>
     );
 }
