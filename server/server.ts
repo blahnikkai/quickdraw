41287989f7aa801f1814484447120800c7228df0
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-08-31 14:10:45 -0400

    Open server to traffic from all ip addresses (0.0.0.0) and use the AWS EC2 public ip instead of implicit localhost for connecting to sockets in the frontend.

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,15 @@
-import express from 'express'
-import { createServer } from 'node:http'
-import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
-import SocketManager from './SocketManager.js'
+import express from "express";
+import { createServer } from "node:http";
+import SocketManager from "./SocketManager.js";
 
-const app = express()
-const server = createServer(app)
-const socketManager = new SocketManager(server)
+const app = express();
+const server = createServer(app);
+const socketManager = new SocketManager(server);
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+socketManager.listen();
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'))
-})
-
-socketManager.listen()
-
-server.listen(3001, () => {
-    console.log('server running at http://localhost:3001')
-})
+const ip = "0.0.0.0";
+const port = 3001;
+server.listen(port, ip, () => {
+    console.log(`server running at http://${ip}:${port}`);
+});
